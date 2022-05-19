33c07de8af0be342221c68b31b285afa655eaad7
salpizar <salpizar@outcoding.com>
2022-05-18 22:48:12 -0600

    Added play interaction button

---
+++ Segundo Proyecto VR/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMainScene() {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+    }
+}
