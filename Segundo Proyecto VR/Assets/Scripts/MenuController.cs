1c6826f9864bc70f02ffa5761252e474115f9f1e
salpizar <salpizar@outcoding.com>
2022-05-24 21:43:32 -0600

    Added menu sections for volume, how to play and about us

--- Segundo Proyecto VR/Assets/Scripts/MenuController.cs
+++ Segundo Proyecto VR/Assets/Scripts/MenuController.cs
@@ -1,11 +1,13 @@
-using System.Net.Mime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MenuController : MonoBehaviour
-{
+public class MenuController : MonoBehaviour {
+
+    public GameObject soundPanel;
+    public GameObject aboutUsPanel;
+    public GameObject howToPlayPanel;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +26,46 @@ public class MenuController : MonoBehaviour
 
     public void ExitApp(){
         Application.Quit();
+    }
+
+    public void ShowVolumeMenu() {
+        DeactivateAllPanels();
+        soundPanel.SetActive(true);
+    }
+
+    public void ShowAboutUs() {
+        DeactivateAllPanels();
+        aboutUsPanel.SetActive(true);
+    }
+
+    public void ShowHowToPlay() {
+        DeactivateAllPanels();
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void IncreaseVolume(Text text) {
+        var currentValue = float.Parse(text.text);
+        float newValue;
+        if (currentValue<1.0f) {
+            newValue = currentValue+0.1f;
+            AudioListener.volume = newValue;
+            text.text = newValue.ToString("0.0");
+        }
+    }
+    
+    public void DecreaseVolume(Text text) {
+        var currentValue = float.Parse(text.text);
+        float newValue;
+        if (currentValue>0.0f) {
+            newValue = currentValue-0.1f;
+            AudioListener.volume = newValue;
+            text.text = newValue.ToString("0.0");
+        }
+    }
+
+    private void DeactivateAllPanels() {
+        soundPanel.SetActive(false);
+        aboutUsPanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
     }
 }
